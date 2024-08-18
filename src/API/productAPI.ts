69d1d9c14fa4c8a2ff846880504b70ce4d3f2cb7
Ấn Trần <tquocan04@gmail.com>
2024-08-19 01:16:05 +0700

    api

--- src/API/productAPI.ts
+++ src/API/productAPI.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import API_URL from './LinkAPI';
 
+//Product
 export const getProducts = async () => {
     const response = await axios.post(`${API_URL}/product`);
     return response.data;
@@ -13,11 +14,6 @@ export const addNewProduct = async (itemdata: FormData) => {
     });
     return response.data;
 };
-export const getCategories = async () => {
-    const response = await axios.post(`${API_URL}/category`);
-    console.log(response.data)
-    return response.data;
-};
 
 export const deleteProduct = async (productId:any) => {
     console.log(productId);
@@ -25,15 +21,33 @@ export const deleteProduct = async (productId:any) => {
     return response.data;
 };
 
+export const updateProduct = async (product: FormData) => {
+    console.log(product);
+    const response = await axios.post(`${API_URL}/admin/product/updateProduct`, product, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    });
+    return response.data;
+};
+
+//Category
+export const getCategories = async () => {
+    const response = await axios.post(`${API_URL}/category`);
+    console.log(response.data)
+    return response.data;
+};
+
+
+//Supplier
 export const getSupplier = async () => {
     const response = await axios.post(`${API_URL}/admin/supplier/getSupplier`);
     console.log("ok:",response.data)
     return response.data;
 };
 
-export const updateProduct = async (product: FormData) => {
-    console.log(product);
-    const response = await axios.post(`${API_URL}/admin/product/updateProduct`, product, {
+export const addNewSupplier = async (supdata: FormData) => {
+    const response = await axios.post(`${API_URL}/admin/supplier/addNewSupplier`, supdata, {
         headers: {
             'Content-Type': 'multipart/form-data',
         },
